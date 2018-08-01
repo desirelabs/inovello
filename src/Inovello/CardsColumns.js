65781239ced31200f265df20fd7f5053e6ef8c1f
desirelabs <francklebas@protonmail.ch>
2018-08-01 13:31:27 +0200

    implement add to list form actions

--- src/Inovello/CardsColumns.js
+++ src/Inovello/CardsColumns.js
@@ -7,14 +7,18 @@ class CardsColumns extends Component {
     super()
     this.state = {
       columns: [],
-      inputValue: ''
+      inputValue: '',
+      toggle: false
     }
   }
 
-  addColumn(columnName) {
-    this.setState({
-      columns: [...this.state.columns, columnName]
-    })
+  addColumn = () => {
+    if (this.state.inputValue) {
+      this.setState({
+        columns: [...this.state.columns, this.state.inputValue],
+        inputValue: ''
+      })
+    }
   }
 
   onChange = e => {
@@ -23,17 +27,25 @@ class CardsColumns extends Component {
     })
   }
 
+  onToggle = bool => {
+    this.setState({
+      toggle: bool
+    })
+  }
+
   render() {
     return (
       <Row>
         {this.state.columns.map((column, i) => (
           <Col xs={3} key={i}>
-            colonne {i}
+            {column}
           </Col>
         ))}
         <Col xs={3}>
           <CreateColumn
             columnsCount={this.state.columns.length}
+            toggle={this.state.toggle}
+            onToggle={this.onToggle}
             addColumn={this.addColumn}
             onChange={this.onChange}
             inputValue={this.state.inputValue}
--- src/Inovello/CreateColumn.js
+++ src/Inovello/CreateColumn.js
@@ -1,11 +1,35 @@
 import React from 'react'
-import { Input } from 'reactstrap'
+import { Input, Button } from 'reactstrap'
 
-const CreateColumn = ({ columnsCount, addColumn, onChange, inputValue }) => (
+const CreateColumn = ({ columnsCount, addColumn, onChange, inputValue, toggle, onToggle }) => (
   <div>
-    <a href="#">{columnsCount === 0 ? '+ Ajouter une liste' : '+ Ajouter une autre liste '}</a>
-    <Input onChange={onChange} value={inputValue} />
+    {toggle ? (
+      <div>
+        <Input onChange={onChange} value={inputValue} onKeyPress={e => e.key === 'Enter' && addColumn()} />
+        <div style={{ display: 'flex' }}>
+          <Button onClick={() => addColumn()}>Ajouter un liste</Button>
+          <a href="#" onClick={() => onToggle(false)} style={closeButton}>
+            &times;
+          </a>
+        </div>
+      </div>
+    ) : (
+      <a href="#" onClick={() => onToggle(true)}>
+        {columnsCount === 0 ? '+ Ajouter une liste' : '+ Ajouter une autre liste '}
+      </a>
+    )}
   </div>
 )
+
+const closeButton = {
+  width: '37px',
+  height: '37px',
+  fontSize: '28px',
+  lineHeight: '28px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  textDecoration: 'none'
+}
 
 export default CreateColumn
